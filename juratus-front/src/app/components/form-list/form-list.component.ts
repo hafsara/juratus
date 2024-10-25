73c8a96d4d713cb650f6cb3175ddcb7852f0998f
root <hafsa.raii@um5s.net.ma>
2024-10-25 18:16:39 +0000

    test

---
+++ juratus-front/src/app/components/form-list/form-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-list.component.html',
+  styleUrl: './form-list.component.scss'
+})
+export class FormListComponent {
+
+}
