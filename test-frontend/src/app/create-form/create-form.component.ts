45f6f7270489ea8e154f2e903e203ed5304fb277
hafsa <hafsaraii>
2024-10-26 00:22:36 +0200

    test

--- test-frontend/src/app/create-form/create-form.component.ts
+++ test-frontend/src/app/create-form/create-form.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './create-form.component.scss'
 })
 export class CreateFormComponent {
+  formTitle: string = '';  // Titre du formulaire
+  fields: Array<{ type: string, value: string }> = [];  // Liste des champs du formulaire
 
+  constructor() {}
+
+  // Ajouter un champ de type donné
+  addField(fieldType: string) {
+    if (this.fields.length < 10) {
+      this.fields.push({ type: fieldType, value: '' });
+    } else {
+      alert("Vous ne pouvez pas ajouter plus de 10 champs.");
+    }
+  }
+
+  // Soumettre le formulaire et afficher les données
+  submitForm() {
+    console.log('Formulaire soumis:', { title: this.formTitle, fields: this.fields });
+    // Ici, tu pourrais envoyer les données à un backend avec HttpClient
+  }
 }
