d2b1f83a9cc8cb97ca779201a7fb5217d1349e19
root <hafsa.raii@um5s.net.ma>
2024-10-25 22:11:11 +0000

    test

--- test-frontend/src/app/app.module.ts
+++ test-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { ExempleComponent } from './exemple/exemple.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    ExempleComponent
   ],
   imports: [
     BrowserModule,
---
+++ test-frontend/src/app/exemple/exemple.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemple',
+  templateUrl: './exemple.component.html',
+  styleUrl: './exemple.component.scss'
+})
+export class ExempleComponent {
+
+}
