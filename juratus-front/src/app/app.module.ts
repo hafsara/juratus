d20eeab975593e9b1a88fe1cf4fafbb8d935dbe4
hafsa <hafsaraii>
2024-10-25 23:12:23 +0200

    test

--- juratus-front/src/app/app.module.ts
+++ juratus-front/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CreateFormComponent } from './components/create-form/create-form.component';
-import { FormListComponent } from './components/form-list/form-list.component';
+// import { CreateFormComponent } from './components/create-form/create-form.component';
+// import { FormListComponent } from './components/form-list/form-list.component';
 import { FormDetailComponent } from './components/form-detail/form-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateFormComponent,
-    FormListComponent,
+    // CreateFormComponent,
+    // FormListComponent,
     FormDetailComponent
   ],
   imports: [
@@ -20,7 +20,6 @@ import { FormDetailComponent } from './components/form-detail/form-detail.compon
     FormsModule,
     HttpClientModule
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],  // Ajoute ceci temporairement
   providers: [],
   bootstrap: [AppComponent]
 })
